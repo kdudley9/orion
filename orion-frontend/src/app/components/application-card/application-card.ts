a3f309a47722c43d115ee5194b891006c4043837
Kenneth Dudley <kennethdudley18@gmail.com>
2025-09-15 22:12:24 -0400

    Add delete functionality to application card

--- orion-frontend/src/app/components/application-card/application-card.ts
+++ orion-frontend/src/app/components/application-card/application-card.ts
@@ -1,13 +1,27 @@
 import { Component, input } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { Application } from '../../models/application';
+import { ApplicationDetailsService } from '../../services/application-details-service';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-application-card',
-  imports: [MatIcon],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './application-card.html',
   styleUrl: './application-card.css'
 })
 export class ApplicationCard {
   application = input.required<Application>();
+
+  constructor(private applicationDetailsService: ApplicationDetailsService, private router: Router) {}
+
+  deleteApplication(applicationId: number) {
+    this.applicationDetailsService.deleteApplication(applicationId).subscribe();
+  }
+
+  navigateToApplicationDetails(applicationId: number) {
+    this.router.navigate([`/applications/${applicationId}`]);
+  }
 }
