8b8e6a6a10970ad93ecf105dbdedf9919db967f7
Kenneth Dudley <kennethdudley18@gmail.com>
2025-11-05 21:52:26 -0500

    Update styling

--- orion-frontend/src/app/shared-components/details-container/details-container.ts
+++ orion-frontend/src/app/shared-components/details-container/details-container.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { ApplicationInfo } from "../../pages/application-info/application-info";
 import { InterviewQuestionPage } from "../../pages/interview-question-page/interview-question-page";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-details-container',
-  imports: [CommonModule, RouterOutlet, ApplicationInfo, InterviewQuestionPage],
+  imports: [CommonModule, RouterOutlet, ApplicationInfo, InterviewQuestionPage, MatIconModule, RouterLinkWithHref],
   templateUrl: './details-container.html',
   styleUrl: './details-container.css'
 })
