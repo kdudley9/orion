e4a07e0c996c6bd6a14ad44519849367b8236800
Kenneth Dudley <kennethdudley18@gmail.com>
2025-11-09 22:12:27 -0500

    Add status colors

--- orion-frontend/src/app/components/application-card/application-card.ts
+++ orion-frontend/src/app/components/application-card/application-card.ts
@@ -5,13 +5,20 @@ import { ApplicationListService } from '../../services/application-list-service'
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { RemoveUnderscoresPipe } from "../../pipes/remove-underscores-pipe";
 import { PatchRequest } from '../../models/patch-request';
 
 @Component({
   selector: 'app-application-card',
-  imports: [MatIconModule, MatButtonModule, MatMenuModule, RemoveUnderscoresPipe, DatePipe, RouterLink],
+  imports: [
+    MatIconModule, 
+    MatButtonModule, 
+    MatMenuModule, 
+    RemoveUnderscoresPipe, 
+    DatePipe, 
+    TitleCasePipe,
+    RouterLink],
   templateUrl: './application-card.html',
   styleUrl: './application-card.css'
 })
--- orion-frontend/src/app/pages/application-info/application-info.ts
+++ orion-frontend/src/app/pages/application-info/application-info.ts
@@ -4,10 +4,11 @@ import { Application } from '../../models/application';
 import { ApplicationListService } from '../../services/application-list-service';
 import { RemoveUnderscoresPipe } from "../../pipes/remove-underscores-pipe";
 import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-application-info',
-  imports: [RemoveUnderscoresPipe, MatIcon, MatIconModule],
+  imports: [RemoveUnderscoresPipe, DatePipe, TitleCasePipe, MatIcon, MatIconModule],
   templateUrl: './application-info.html',
   styleUrl: './application-info.css'
 })
@@ -27,7 +28,8 @@ export class ApplicationInfo implements OnInit {
     archived: false
   }
 
-  @ViewChild('companyAvatar', { static: false }) elementRef!: ElementRef;
+  @ViewChild('status', { static: false }) statusElementRef!: ElementRef;
+  @ViewChild('companyAvatar', { static: false }) avatarElementRef!: ElementRef;
   logoText: string | null = null;
   
   constructor(private applicationListService: ApplicationListService, private route: ActivatedRoute) {}
@@ -37,7 +39,39 @@ export class ApplicationInfo implements OnInit {
     this.applicationListService.getApplication(this.applicationId).subscribe((data) => {
       this.application = data;
       this.logoText = this.applicationListService.companyLogo(this.application.company);
-      this.elementRef.nativeElement.style.background = this.applicationListService.companyLogoColor(this.logoText);
+      this.avatarElementRef.nativeElement.style.background = this.applicationListService.companyLogoColor(this.logoText);
+      this.statusColors(this.application.status);
     });
+  }
+
+  statusColors(status: string | undefined): void {
+    let statusStyle = this.statusElementRef.nativeElement.style;
+
+    switch(status) {
+      case 'APPLIED':
+        statusStyle.background = 'var(--applied-background)';
+        statusStyle.color = 'var(--applied-text)';
+        break;
+      case 'ONLINE_ASSESSMENT':
+        statusStyle.background = 'var(--assessment-background)';
+        statusStyle.color = 'var(--assessment-text)';
+        break;
+      case 'INTERVIEW':
+        statusStyle.background = 'var(--interview-background)';
+        statusStyle.color = 'var(--interview-text)';
+        break;
+      case 'OFFER':
+        statusStyle.background = 'var(--offer-background)';
+        statusStyle.color = 'var(--offer-text)';
+        break;
+      case 'REJECTED':
+        statusStyle.background = 'var(--rejected-background)';
+        statusStyle.color = 'var(--rejected-text)';
+        break;
+      case 'GHOSTED':
+        statusStyle.background = 'var(--ghosted-background)';
+        statusStyle.color = 'var(--ghosted-text)';
+        break;
+    }
   }
 }
